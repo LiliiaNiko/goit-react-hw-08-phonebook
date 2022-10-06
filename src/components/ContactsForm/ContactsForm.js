30ef9e0de5c6cdb891625328087dcad61d002343
LiliiaNikolaichuk <l.nikolaychuk@ukr.net>
2022-10-06 22:52:37 +0300

    update

--- src/components/ContactsForm/ContactsForm.js
+++ src/components/ContactsForm/ContactsForm.js
@@ -1,64 +1,45 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/operations';
+import { selectContacts } from 'redux/selectors';
 
 import { Form, Input, Button } from './ContactsForm.styled';
 
 export const ContactsForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
   const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    //console.log('on submit name:', name, number);
+  const handleSubmit = evt => {
+    evt.preventDefault();
 
-    dispatch(addContact(name, number));
-    reset();
-  };
+    const form = evt.target;
 
-  const reset = () => {
-    setName('');
-    setNumber('');
-  };
+    const name = evt.target.elements.name.value;
+    const phone = evt.target.elements.phone.value;
 
-  const handleChange = e => {
-    switch (e.target.name) {
-      case 'name':
-        setName(e.target.value);
-        break;
-      case 'number':
-        setNumber(e.target.value);
-        break;
-      default:
-        return;
-    }
+    contacts.find(contact => contact.name === name)
+      ? alert(`${name} is already in contacts`)
+      : dispatch(addContact({ name, phone }));
+
+    form.reset();
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <label htmlFor={name.id}>Name</label>
+      <label>Name</label>
       <Input
-        id={name.id}
         type="text"
         name="name"
-        value={name}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        onChange={handleChange}
       />
-      <label htmlFor={number.id}>Number</label>
+      <label>Number</label>
       <Input
-        id={number.id}
         type="tel"
-        name="number"
-        value={number}
+        name="phone"
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
-        onChange={handleChange}
       />
       <Button type="submit">Add contact</Button>
     </Form>
